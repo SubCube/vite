8907a10d3093648f6a98ebd21753e4a067abd155
Максим Бельфер <maxbelf1@gmail.com>
2022-10-21 22:34:43 +0300

    feat: added pinia

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,13 @@
+<script lang="ts" setup>
+import { useTrialStore } from '@/stores/index'
+import { storeToRefs } from 'pinia'
+
+const { increment } = useTrialStore()
+const { counter } = storeToRefs(useTrialStore())
+</script>
+
 <template>
   <h1>Main page</h1>
+  <div>{{ counter }}</div>
+  <button @click="increment()">Increment</button>
 </template>
-
-<script lang="ts" setup></script>
